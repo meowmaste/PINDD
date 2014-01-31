1a6a54e2e95f9c03b772d88249ff6c7e92178ee1
Joslyn <jrosbrook@gmail.com>
2014-01-30 17:41:38 -0800

    init commit w/ postgres

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pindd::Application.config.secret_key_base = 'cb91eae1ab8f7e72c6546ab8cbc4608e2a044e5d06d311da194fcc26bf07968e0522f0e564312a1cf5e2299aad532d4adcc9b2269dc5ff7217e43b2e85d91489'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Pindd::Application.config.session_store :cookie_store, key: '_pindd_session'
