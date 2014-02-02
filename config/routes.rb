4777f48ede36bea1ce4a623ee58564f1b4aceb7f
Joslyn <jrosbrook@gmail.com>
2014-02-02 09:53:22 -0800

    add devise, root to sessions#new in devise scope

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Pindd::Application.routes.draw do
+  devise_for :users
   resources :notes
+
+  devise_scope :user do
+    get "/", :to => "devise/sessions#new"
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
