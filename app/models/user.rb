13943b9d612fc73ae68e819b758a3bae449ea49a
Lillian Ng <lillian.k.ng@gmail.com>
2014-02-04 13:42:17 -0800

    added default group

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,18 @@ class User < ActiveRecord::Base
   has_and_belongs_to_many :groups
 
   delegate :notes, to: :groups
+
+  after_create :create_default_group
+
+  def create_default_group
+    group = Group.new
+    group.name = self.email.concat '-default'
+    group.users << self
+    if group.save
+      self.groups << group
+    else
+      # ?????? group does not save so there's no default group
+      binding.pry
+    end
+  end
 end
