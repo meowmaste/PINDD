e96e40d4fb10db7894fe08e0c0f57ae3fb1de89d
Lillian Ng <lillian.k.ng@gmail.com>
2014-02-06 12:22:20 -0800

    DRY notes

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,6 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 @admin = User.create(email: "admin@example.com",
-                     password: "password"
+                     password: "password",
                      password_confirmation: "password",
                      role: "admin")
