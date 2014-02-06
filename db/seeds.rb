b740e8a8c7b072d3207c1939e8b65c16c7454e31
Lillian Ng <lillian.k.ng@gmail.com>
2014-02-06 01:01:26 -0800

    changed checkbox test

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+@admin = User.create(email: "admin@example.com",
+                     password: "password"
+                     password_confirmation: "password",
+                     role: "admin")
--- test/features/notes/checking_a_note_test.rb
+++ test/features/notes/checking_a_note_test.rb
@@ -1,8 +1,24 @@
 require "test_helper"
 
 feature "Notes / Checking A Note" do
-  scenario "checkbox test (NOT WRITTEN YET)" do
+  scenario "user can click the check box on a note" do
+    sign_in(:lillian)
     visit root_path
-    page.must_have_content "CHECKBOX"
+    box1 = page.find_by_id("check_#{notes(:lnote).id}")
+    assert !box1.checked?
+    check("check_#{notes(:lnote).id}")
+
+    save_and_open_page
+
+    visit note_path(notes(:lnote))
+    box2 = page.find_by_id("check_#{notes(:lnote).id}")
+    print page.html
+    save_and_open_page
+
+    assert box2.checked?
+
+    ##this test still doesn't work
+
+
   end
 end
