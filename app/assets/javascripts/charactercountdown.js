aa93ea75abee75794de99ca3cecbac77fdfd0477
Joslyn <jrosbrook@gmail.com>
2014-03-04 11:05:26 -0800

    fix dash to underscore typo

--- app/assets/javascripts/charactercountdown.js
+++ app/assets/javascripts/charactercountdown.js
@@ -1,12 +1,11 @@
 function updateCountdown() {
     // 140 is the max message length
-    var remaining = 8000 - jQuery('#note-content').val().length;
+    var remaining = 8000 - jQuery('#note_content').val().length;
     jQuery('.countdown').text(remaining + ' characters remaining.');
 }
 
 jQuery(document).ready(function($) {
     updateCountdown();
-    $('#note-content').change(updateCountdown);
-    $('#note-content').keyup(updateCountdown);
-  
+    $('#note_content').change(updateCountdown);
+    $('#note_content').keyup(updateCountdown);  
 });
