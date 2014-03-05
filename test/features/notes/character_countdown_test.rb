5dcaf62166cf37fc41dfc85a6aeb66f54f3a3913
Joslyn <jrosbrook@gmail.com>
2014-03-05 10:50:28 -0800

    js trouble shooting

--- test/features/notes/character_countdown_test.rb
+++ test/features/notes/character_countdown_test.rb
@@ -5,14 +5,14 @@ feature "Notes / Character Countdown" do
   scenario "user can see character countdown when creating a note", js: true do
     sign_in
     visit new_note_path
-    page.text.must_include "140 characters remaining"
+    save_and_open_page
+    assert page.has_content?("140 characters remaining")
     fill_in "note_content", with: "MEOW MIX MEOW MIX PLEASE DELIVER!"
-    page.text.must_include "107 characters remaining"
+    assert page.has_content?("107 characters remaining")
   end
 
   scenario "user can see character countdown when editing a note", js: true do
-    require_js
-    sign_in(:lillian)
+    sign_in(:joslyn)
     visit edit_note_path(notes(:jnote))
     page.text.must_include "118 characters remaining"
     fill_in "note_content", with: "MEOW MIX MEOW MIX PLEASE DELIVER!"
