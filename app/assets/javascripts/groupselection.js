3656918c9f68a8a99036648d94bcfbd94d7b7b82
Joslyn <jrosbrook@gmail.com>
2014-03-05 17:41:11 -0800

    group selection function and logic to get nav checkmarks to stay checked

--- app/assets/javascripts/groupselection.js
+++ app/assets/javascripts/groupselection.js
@@ -1,3 +1,7 @@
-function groupSelection() {
-  
-}
+jQuery(document).ready(function() {  
+  $("#group-form input").change(function() {
+    var urlVars = $("#group-form").serialize();
+    var url = window.location.origin + window.location.pathname + "?" + urlVars;
+    window.location = url
+  })
+});
