0a6cea22fdc34e8688ea540cf21205acf3f4d4ed
Joslyn <jrosbrook@gmail.com>
2014-03-06 15:35:39 -0800

    sendtext controller

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -1,13 +1,22 @@
 require 'twilio-ruby'
 
-class TwilioController < ApplicationController
-  include Webhookable
-
-  after_filter :set_header
-
-  def text
-    response = Twilio::TwiML::Response.new do |r|
-      r.say 'Hey there. Twilio Integration with Rails 4'
-    end 
+class SendTextController < ApplicationController
+  def index
   end 
-end
+
+  def send_text_message
+    number_to_send_to = params[:number_to_send_to]
+
+    twilio_sid = ""
+    twilio_token = ""
+    twilio_phone_number = ""
+
+    @twilio_client = Twilio::REST::Client.new twilio_sid, twilio_token
+    
+    @twilio_client.account.sms.messages.create(
+      :from => "+1#{twilio_phone_number}", 
+      :to => number_to_send_to,
+      :body => "This is a message. It gets sent to #{number_to_send_to}"
+      )
+  end 
+end 
