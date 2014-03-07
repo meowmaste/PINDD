115906a5eca72dd40e52aba40efe662e592eb784
Joslyn <jrosbrook@gmail.com>
2014-03-06 20:21:58 -0800

    fix vertical scrolling on note

--- app/helpers/notes_helper.rb
+++ app/helpers/notes_helper.rb
@@ -1,22 +1,22 @@
 module NotesHelper
-	def note_color note
-		if note.my_note?
-			"rgb(255,255,136)"
-		else
-			note.group.color || "#ADC1D6"
-		end
-	end
+  def note_color note
+    if note.my_note?
+      "rgb(255,255,136)"
+    else
+      note.group.color || "#ADC1D6"
+    end
+  end
 
   def font_size note
-    if note.content.length < 20
-      return "font-size:50px;line-height:55px"
-    elsif note.content.length < 50
-      return "font-size:35px;line-height:40px"
-    elsif note.content.length < 100
-      return "font-size:25px;line-height:30px"
-    elsif note.content.length < 150
-      return "font-size:15px;line-height:20px"
+    if note.content.length < 25
+      return "font-size:40px;line-height:45px"
+    elsif note.content.length < 55
+      return "font-size:32px;line-height:35px"
+    elsif note.content.length < 75
+      return "font-size:24px;line-height:30px"
+    elsif note.content.length < 105
+      return "font-size:22px;line-height:25px"
     end
-    "font-size:20px;line-height:22px"
+      "font-size:18px;line-height:20px"
   end
 end
