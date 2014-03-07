f8ecb803144f3776f6cc25c6fdebd3f7d41ff6cd
amanda spiegel <amandakspiegel@gmail.com>
2014-03-06 16:07:53 -0800

    font sizes are adjusted to look less assy

--- app/helpers/notes_helper.rb
+++ app/helpers/notes_helper.rb
@@ -11,9 +11,11 @@ module NotesHelper
     if note.content.length < 20
       return "font-size:50px;line-height:55px"
     elsif note.content.length < 50
-      return "font-size:40px;line-height:45px"
+      return "font-size:35px;line-height:40px"
     elsif note.content.length < 100
-      return "font-size:30px;line-height:35px"
+      return "font-size:25px;line-height:30px"
+    elsif note.content.length < 150
+      return "font-size:15px;line-height:20px"
     end
     "font-size:20px;line-height:22px"
   end
