c9da640da98ee474910d73c722e23d1771d6f595
Joslyn <jrosbrook@gmail.com>
2014-03-06 20:06:48 -0800

    note shouldn't vertically scroll fix

--- app/helpers/notes_helper.rb
+++ app/helpers/notes_helper.rb
@@ -9,13 +9,14 @@ module NotesHelper
 
   def font_size note
     if note.content.length < 25
-      return "font-size:50px;line-height:55px"
-    elsif note.content.length < 50
       return "font-size:40px;line-height:45px"
+    elsif note.content.length < 55
+      return "font-size:32px;line-height:35px"
     elsif note.content.length < 75
-      return "font-size:30px;line-height:35px"
-    elsif note.content.length < 100
+      return "font-size:24px;line-height:30px"
+    elsif note.content.length < 105
+      return "font-size:22px;line-height:25px"
     end
-    "font-size:18px;line-height:20px"
+      "font-size:18px;line-height:20px"
   end
 end
